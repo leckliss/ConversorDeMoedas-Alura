00b626b7171fd236dbb8f1f1f0eb39266a8e879e
leckliss <erickcaique150@hotmail.com>
2024-07-01 19:38:17 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import com.google.gson.Gson;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean sair = false;
+        while(sair == false){
+
+            System.out.println("Olá seja bem-vindo ao conversor de moedas");
+            System.out.println("Digite qual moeda você deseja converter");
+
+            
+
+            if (sair == true){
+                break;
+            }
+        }
+    }
+}
